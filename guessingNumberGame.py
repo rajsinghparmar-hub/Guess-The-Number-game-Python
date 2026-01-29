e1ba23c0c19623eee64873f6119f0e39b42ab09e
rajsinghparmar-hub <parmarrajsp@gmail.com>
2026-01-29 23:07:45 +0530

    first commit

---
+++ guessingNumberGame.py
@@ -0,0 +1,17 @@
+import random as ran
+
+target = ran.randint(1, 100)
+
+while True:
+    usrChoice = input("Guess the target or quit(Q): ")
+    if usrChoice == "Q":
+        break
+    usrChoice = int(usrChoice)
+    if usrChoice == target:
+        print("Success: correct guess")
+        break
+    elif usrChoice < target:
+        print("your number was too small, take a bigger guess: ")
+    else: 
+        print("your number is too big, take a small guess: ")
+print("Game over") 
